2bdd46f5e3f212a1c84a85a22ea402475ec7b389
Austin Daelemans <austin.daelemans@b-yond.com>
2018-07-11 10:45:58 -0500

    New hello message again for red black Updated for highlander

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello GCP CD, This is a Red/Black image Updated! Check that old instances are scaled down")
+	io.WriteString(w, "Hello GCP CD, This is a Red/Black image Updated! Highlander Test")
 }
 
 func main() {
