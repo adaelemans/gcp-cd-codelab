806762963bafa9bea975c16bb273f89d816c553d
Austin Daelemans <austin.daelemans@b-yond.com>
2018-07-11 10:21:52 -0500

    New hello message again for red black Updated

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello GCP CD, This is a Red/Black image!")
+	io.WriteString(w, "Hello GCP CD, This is a Red/Black image Updated!")
 }
 
 func main() {
